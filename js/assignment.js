a29228494f1ee621d008cf911eb6ebbec60663c6
Alex Javier <alexavier1989@gmail.com>
2024-12-17 23:17:15 -0500

    feat: Tasks part 2 done! Variables were created and functions updated

--- js/assignment.js
+++ js/assignment.js
@@ -1,41 +1,26 @@
 "use strict";
 
-// Declaring constant variables to store references to the elements
-// that will be updated by your code.
 const bookAnswerEl = document.getElementById("book-answer")
 const activityAnswerEl = document.getElementById("activity-answer")
 const submissionBtn = document.getElementById("submission-btn")
 
-
-// Declare a constant variable named "book" and assign
-// the name of your favorite book to that variable.
-
-
-// Declare a mutable variable named "activity" and assign
-// an activity to it.
-
+const book = "The Psychology of Money"
+let activity = "Play basketball"
 
 
 function updateBookSentence() {
-    // Sets the text inside the element with the id of book answer. 
-    // innerText is one of many properties on a HTML element.
     bookAnswerEl.innerText = book
 }
 
 function updateActivitySentence() {
-    // Update this function 
-    // (hint: it will be similiar to the previous function)
-
+    activityAnswerEl.innerText = activity
 }
 
 function render() {
-    // Finish writing this function
     updateBookSentence();
-
+    updateActivitySentence();
 }
 
 submissionBtn.addEventListener("click", function () {
-    // Calling the function that renders the update
-    // when the button is clicked
     render();
 })
