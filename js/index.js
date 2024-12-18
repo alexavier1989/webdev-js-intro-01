8852cd03e0d0722b3ff4e7c17664f57266f117e2
Alex Javier <alexavier1989@gmail.com>
2024-12-17 23:07:37 -0500

    feat: Tasks part 1 done! experiments with variables in index.js file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let fiancee = "Maria";
+console.log(fiancee); // Output: Maria
+
+fiancee = "Julia";
+console.log(fiancee); // Output: Julia
+
+const courtshipDuration = 2;
+console.log(courtshipDuration); // Output: 2
+
+courtshipDuration = 5;  // Output: Generate an error - Uncaught TypeError: Assignment to constant variable.
+
+*/
